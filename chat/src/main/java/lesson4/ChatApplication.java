2a28465187d9a810e72c1d3abe7b2ca3dd52d773
robert_gevorgyan <gevorgyanrk@gmail.com>
2020-12-14 19:32:38 +0300

    Чат готов

--- chat/src/main/java/lesson4/ChatApplication.java
+++ chat/src/main/java/lesson4/ChatApplication.java
@@ -14,9 +14,11 @@ public class ChatApplication extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+
         Parent auth = FXMLLoader.load(getClass().getResource("auth.fxml"));
         primaryStage.setTitle("Авторизация");
         primaryStage.setScene(new Scene(auth));
         primaryStage.show();
+
     }
 }
--- chat/src/main/java/lesson4/RegistrationController.java
+++ chat/src/main/java/lesson4/RegistrationController.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 
 public class RegistrationController {
 
+
     public TextField login;
     public TextField password;
 
